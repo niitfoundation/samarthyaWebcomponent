3320907fc97a41d85c9339e124687b068970fd5f
sankhlasaini <sankhlasaini@gmail.com>
2017-03-20 19:12:59 +0530

    working on dialog

--- sam-dynamic-section/sam-dynamic-element/sam-dynamic-element.component.ts
+++ sam-dynamic-section/sam-dynamic-element/sam-dynamic-element.component.ts
@@ -1,19 +1,25 @@
 import { SamDynamicElementBase } from './sam-dynamic-element-base';
 import { Component, OnInit, Input } from '@angular/core';
+import { Md2Dialog } from 'md2'
 
 @Component({
   selector: 'sam-dynamic-element',
   templateUrl: './sam-dynamic-element.component.html',
-  styleUrls: ['./sam-dynamic-element.component.css']
+  styleUrls: ['./sam-dynamic-element.component.css'],
+  providers: [Md2Dialog]
 })
 export class SamDynamicElementComponent implements OnInit {
+
   @Input() element: SamDynamicElementBase<any>;
-  constructor() { }
 
-  ngOnInit() {
-  }
-  public getElement(card:any) {
-    console.log(card);
-  }
+  constructor(public dialog: Md2Dialog) { }
 
+  public formData: any;
+
+  ngOnInit() { }
+
+  onEdit(dialog: Md2Dialog, cardData: any) {
+    console.log(cardData);
+    dialog.open();
+  }
 }
