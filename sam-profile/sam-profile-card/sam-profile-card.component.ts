176be2206b901c4b28412a923b45a843afc76312
dheerendra <dhirudheerendra@gmail.com>
2017-03-31 11:26:45 +0530

    download card  added

--- sam-profile/sam-profile-card/sam-profile-card.component.ts
+++ sam-profile/sam-profile-card/sam-profile-card.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+// import * as html2canvas from 'html2canvas';
 @Component({
   selector: 'sam-profile-card',
   templateUrl: './sam-profile-card.component.html',
@@ -15,5 +16,18 @@ export class SamProfileCardComponent implements OnInit {
   ngOnInit() {
     this.cardData = this.profileCardData;
   }
+  //  public download(event) {
+  //   console.log('asdasd');
+  //   var timeStamp = new Date().getTime();
+  //   console.log(timeStamp);
+  //   var name="samarthya_"+this.cardData['name']+timeStamp+".png";
+  //   html2canvas(document.getElementById("profilecard")).then(function (canvas) {
+  //     var imageData = canvas.toDataURL("image/png");
+  //   var newData = imageData.replace(/^data:image\/png/,"data:application/octet-stream");
+  //   var dwld = document.getElementById("downloadpc");
+  //   dwld.setAttribute("download", name);
+  //   dwld.setAttribute("href", newData);    
+  //   });
+  // }
 }
 
