87cad67a7673b6c3f4bac27fd17e34ba4922f39b
sankhlasaini <sankhlasaini@gmail.com>
2017-04-03 16:12:40 +0530

    form added

--- sam-profile/sam-profile-card/sam-profile-card.component.ts
+++ sam-profile/sam-profile-card/sam-profile-card.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
-// import * as html2canvas from 'html2canvas';
+import * as html2canvas from 'html2canvas';
 @Component({
   selector: 'sam-profile-card',
   templateUrl: './sam-profile-card.component.html',
@@ -16,19 +16,22 @@ export class SamProfileCardComponent implements OnInit {
 
   ngOnInit() {
     this.cardData = this.profileCardData;
+    console.log(this.cardData);
   }
-  //  public download(event) 
-  //   console.log('asdasd');
-  //   var timeStamp = new Date().getTime();
-  //   console.log(timeStamp);
-  //   var name="samarthya_"+this.cardData['name']+timeStamp+".png";
-  //   html2canvas(document.getElementById("profilecard")).then(function (canvas) {
-  //     var imageData = canvas.toDataURL("image/png");
-  //   var newData = imageData.replace(/^data:image\/png/,"data:application/octet-stream");
-  //   var dwld = document.getElementById("downloadpc");
-  //   dwld.setAttribute("download", name);
-  //   dwld.setAttribute("href", newData);
-  //   });
-  // }
+  public download(event: any) {
+    console.log('asdasd');
+    var timeStamp = new Date().getTime();
+    console.log(timeStamp);
+    console.log(event)
+    var name = "samarthya_" + this.cardData['name'] + timeStamp + ".png";
+    html2canvas(document.getElementById("profilecard")).then(function (canvas) {
+      console.log('called')
+      var imageData = canvas.toDataURL("image/png");
+      var newData = imageData.replace(/^data:image\/png/, "data:application/octet-stream");
+      var dwld = document.getElementById("downloadpc");
+      dwld.setAttribute("download", name);
+      dwld.setAttribute("href", newData);
+    });
+  }
 }
 
